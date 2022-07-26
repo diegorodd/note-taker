901473cab0db2d399ddc4cfa519b4c1dc08289fb
diegorodd <dieegoa@icloud.com>
2022-07-26 18:53:25 -0400

    added the delete section

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -11,6 +11,20 @@ router.get('/notes', (req, res) => {
     .catch((err) => res.status(500).json(err));
 });
 
-
+router.post('/notes', (req, res) => {
+    store
+      .addNote(req.body)
+      .then((note) => res.json(note))
+      .catch((err) => res.status(500).json(err));
+  });
+  
+//  deletes notes by id
+  router.delete('/notes/:id', (req, res) => {
+    store
+      .removeNote(req.params.id)
+      .then(() => res.json({ ok: true }))
+      .catch((err) => res.status(500).json(err));
+  });
+  
 
 module.exports = router;
