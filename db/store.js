3f76eb5ec0b7c3a86ecc9f753066cd4643014997
diegorodd <dieegoa@icloud.com>
2022-07-28 17:30:41 -0400

    made the getnotes function

--- db/store.js
+++ db/store.js
@@ -13,9 +13,22 @@ write(note){
    }
 
    getNotes(){
+       return this.read().then((notes)=> {
+           let notesArray;
+        //    try catch...https://www.w3schools.com/java/java_try_catch.asp
+        try {
+            // using json.parse to take the json data to turn it into a js object and then concat into an array
+            notesArray = [].concat(JSON.parse(notes));
+        } catch(err){
+            // if error happens return an empty array.
+            notesArray = [];
+        }
+        return notesArray
+       })
 
    }
    addNote(note){
+    //    object destructuring title and text are like properties of a note object.
     const { title, text } = note;
     if (!title || !text) {
         throw new Error('cant be blank');
@@ -24,8 +37,10 @@ write(note){
     const newNote = { title, text, id: uuidv4() };
 
     // add the new note 
-    return this.retrieveNotes()
+    return this.getNotes()
+    // using a spread operator we are putting current and new notes into an array 
         .then(notes => [...notes, newNote])
+        // updated notes will be adding to the db.json
         .then(updatedNotes => this.write(updatedNotes))
         .then(() => newNote);
 
